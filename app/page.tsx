594fa67370038872b559dbf52daf3674053c6c38
madeihaarshad <madeihasatti08@gmail.com>
2026-05-18 16:21:49 +0500

    Lab 8 initial commit

---
+++ app/page.tsx
@@ -0,0 +1,46 @@
+export default function Home() {
+  return (
+    <main
+      style={{
+        textAlign: "center",
+        marginTop: "10%",
+        fontFamily: "Arial, sans-serif",
+        backgroundColor: "#111827",
+        height: "100vh",
+        paddingTop: "50px",
+      }}
+    >
+      <h1
+        style={{
+          fontSize: "3rem",
+          fontWeight: "bold",
+          color: "#3b82f6",
+          marginBottom: "30px",
+        }}
+      >
+        Lab 8 — Vercel Deployment
+      </h1>
+
+      <p
+        style={{
+          fontSize: "1.8rem",
+          fontWeight: "bold",
+          color: "white",
+          marginBottom: "15px",
+        }}
+      >
+        Name: Madeiha Arshad
+      </p>
+
+      <p
+        style={{
+          fontSize: "1.8rem",
+          fontWeight: "bold",
+          color: "white",
+        }}
+      >
+        Roll No: 01-131232-040
+      </p>
+    </main>
+  );
+}
