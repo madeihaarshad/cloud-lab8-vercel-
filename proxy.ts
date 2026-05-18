840fb1cf9041997a2791e2596bd9e11a92c00145
madeihaarshad <madeihasatti08@gmail.com>
2026-05-18 22:39:45 +0500

    Migrate middleware to proxy

--- proxy.ts
+++ proxy.ts
@@ -1,7 +1,7 @@
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
-export function middleware(request: NextRequest) {
+export function proxy(request: NextRequest) {
   const res = NextResponse.next();
   res.headers.set("x-lab", "Lab8-CloudComputing");
   console.log(`[EDGE] ${request.method} ${request.nextUrl.pathname}`);
